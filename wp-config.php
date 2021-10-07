a06271003d551d367b9904de42f96b6dc7f2f55b
unknown <yogeshscorpsoft3630@gmail.com>
2021-10-08 00:21:55 +0530

    add db password

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'inflight_shopping' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Amit@123' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
